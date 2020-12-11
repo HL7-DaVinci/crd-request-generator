94e0aede366e0061e8ed3b1ffd91038088276e0f
kghoreshi <keeyan.ghoreshi@uconn.edu>
2020-12-11 03:02:04 -0500

    deal with base url

--- src/util/buildRequest.js
+++ src/util/buildRequest.js
@@ -1,5 +1,5 @@
 
-function buildR4Request(request, patient, ehrUrl, token, prefetch, includePrefetch, hook) {    
+export default function buildRequest(request, patient, ehrUrl, token, prefetch, includePrefetch, hook) {    
     const r4json = {
         "hookInstance": "d1577c69-dfbe-44ad-ba6d-3e05e953b2ea",
         "fhirServer": ehrUrl.r4,
@@ -75,67 +75,4 @@ function buildR4Request(request, patient, ehrUrl, token, prefetch, includePrefet
     console.log(r4json);
     console.log("--------- r4");
     return r4json;
-}
-
-function buildStu3Request(request, patient, ehrUrl, token, prefetch, includePrefetch, hook) {    
-    const stu3json = {
-        "hook": hook,
-        "hookInstance": "63d7d1fa-9469-4c44-a5f7-76a129e30967",
-        "fhirServer": ehrUrl.stu3,
-        "fhirAuthorization": null,
-        "user": "Practitioner/1234",
-        "context": {
-          "patientId": patient.id,
-          "encounterId": null,
-          "services": null
-        }
-    };
-
-    if (hook == "order-review") {
-        stu3json.context.orders = {
-            "resourceType": "Bundle",
-            "entry": [
-                request
-            ]
-        }
-    } else if (hook == "order-select") {
-        stu3json.context.draftOrders = {
-            "resourceType": "Bundle",
-            "entry": [
-                request
-            ]
-        }
-        stu3json.context.selections = [
-            request.resourceType + "/" + request.id
-        ]
-    } else if (hook == "order-sign") {
-        stu3json.context.draftOrders = {
-            "resourceType": "Bundle",
-            "entry": [
-                request
-            ]
-        }
-    }
-
-    if (includePrefetch) {
-        stu3json.prefetch = {
-            "deviceRequestBundle":{
-                "resourceType": "Bundle",
-                "type": "collection",
-                "entry": prefetch
-            }
-        }
-    }
-
-    console.log(stu3json);
-    console.log("--------- stu3");
-    return stu3json;
-}
-
-export default function buildRequest(request, patient, ehrUrl, token, prefetch, version, includePrefetch, hook) {
-    if (version==="stu3") {
-        return buildStu3Request(request, patient, ehrUrl, token, prefetch, includePrefetch, hook);
-    } else if (version==="r4") {
-        return buildR4Request(request, patient, ehrUrl, token, prefetch, includePrefetch, hook);
-    }
 }
